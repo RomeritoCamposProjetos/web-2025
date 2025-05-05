4e46c53b5ad30c5076326966c121918038739630
Romerito Campos de Andrade <romerito.campos@gmail.com>
2025-05-05 15:55:34 -0300

    Cookies - Pronto

--- slides/04/exemplo2/app.py
+++ slides/04/exemplo2/app.py
@@ -1,26 +1,43 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, make_response, render_template, request
 
-app = Flask (__name__)
+app = Flask(__name__)
 
-@app.route('/')
+@app.route("/")
 def index():
-    return render_template('index.html')
+    return render_template('cookies.html')
 
-@app.route('/formularios')
-def formularios():
-    return render_template('formulario.html')
 
-@app.route('/form1', methods=['POST', 'GET'])
-def form1():
-    if request.method == 'POST':
-        name = request.form['name']    
-        return "Você enviou o nome: " + name
-    else:
-        return redirect(url_for('formularios'))
-    
-    
-@app.route('/form2', methods=['GET', 'POST'])
-def form2():
-    data = request.args.get('opcao')
-    return data
-    
+# definindo cookies de sessão
+# aqui ele expira quando o navegador for encerrado
+@app.route("/cookie1")
+def cookie1():
+    text = "<h1>Um cookie foi definido<h1/>"
+    response = make_response(text)
+    response.set_cookie('session_cookie', 'teste')
+    return response
+
+# aqui a rota só recebe request POST
+# obtemos o tempo enviado em segundos(não validamos)
+# esse tempo será usado para definir o tempo de vida do cookie
+# este cookie é um cookie permanente
+# o atributo max_age determina o tempo de vida dele
+# mesmo fechando o navegador, não será apagado
+@app.route("/cookie2", methods=['POST'])
+def cookie2():
+    text = "<h1>Um cookie foi definido<h1/>"
+    time = int(request.form['time'])
+    response = make_response(text)
+    response.set_cookie('permanent_cookie', 'teste', max_age=time)
+    return response
+
+
+@app.route("/cookie3", methods=['POST'])
+def cookie3():
+    option = eval(request.form['opcao'])
+    template = render_template('httponly.html', opcao=str(bool(option)), dado='red')
+    response = make_response(template)
+    if 'http_only' in request.cookies:
+        response.delete_cookie(request.cookies['http_only'])
+    response.set_cookie('http_only', str(bool(option)), httponly=bool(option))
+    return response
+
